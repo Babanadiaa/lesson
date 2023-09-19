edcf7a1c93f9ba322b7519319bcc0179df008ca2
BabaNadia <maksumatmn@gmail.com>
2023-09-19 14:55:46 +0300

    dawdaw

---
+++ js/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+
+
+// let numberOfFilms = prompt('скільки фільмів ви вже подивились?');
+// let lastWatchFilm = prompt('який останній фільм ви подивились?');
+// let movieRating = prompt('яку оцінку ви йому поставите?')
+// let movieSetting = lastWatchFilm + ':' + movieRating;
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies:{
+//         movieSetting
+
+//     },
+//     actors:'',
+//     genres:'',
+//     privat: false,
+
+// };
+// console.log(personalMovieDB);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
